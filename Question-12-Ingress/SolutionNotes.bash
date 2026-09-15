bf611bb1d62de667f6dab690a9e57ff146d0160f
AchrafAzouagh <84711357+AchrafAzouagh@users.noreply.github.com>
2026-09-15 21:10:12 +0200

    Update SolutionNotes.bash

--- Question-12-Ingress/SolutionNotes.bash
+++ Question-12-Ingress/SolutionNotes.bash
@@ -3,8 +3,9 @@ kubectl expose deployment echo -n echo-sound --name echo-service --type NodePort
 kubectl get svc -n echo-sound echo-service
 
 #Declarative command to generate yaml
-kubectl create ingress echo --rule="example.org/echo=echo-service:8080" -n echo-sound --dry-run=client -o yaml > ingress.yaml
+#kubectl create ingress echo --rule="example.org/echo=echo-service:8080" -n echo-sound --dry-run=client -o yaml > ingress.yaml
 
+-> manifest in ingress doc first
 # Then edit the yaml like so:
 cat <<'EOF' > ingress.yaml
 apiVersion: networking.k8s.io/v1
